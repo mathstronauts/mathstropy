f9230c1ee15b33129cd6a49392b2a01f5c5277d8
Richard Hamilton <richard.hamilton@gm.com>
2020-08-30 12:13:48 -0400

    Added randomword() and wordmask() functions with unit tests

---
+++ mathstropy/__init__.py
@@ -0,0 +1,3 @@
+from .miscellaneous_functions import randomword
+from .miscellaneous_functions import wordmask
+
---
+++ test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 29 23:44:27 2020
+
+@author: JZST6G
+"""
+
+##import os
+##
+##path_parent = os.path.dirname(os.getcwd())
+##os.chdir(path_parent)
+##print(os.getcwd())
+
+import mathstropy
+
+word = mathstropy.randomword()
+
+print(mathstropy.wordmask(word))
